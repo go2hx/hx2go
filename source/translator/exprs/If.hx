1c3dd5d905d674dbc9cabc9f5d86212fc96c0e52
Joey <9813078+PXshadow@users.noreply.github.com>
2025-12-09 20:20:40 +0100

    exprs: If stmt with translateToBlock
    Co-authored-by: MKI <mikaib@duck.com>

--- source/translator/exprs/If.hx
+++ source/translator/exprs/If.hx
@@ -4,10 +4,27 @@ import translator.Translator;
 import HaxeExpr;
 
 function translateIf(t:Translator, econd:HaxeExpr, eif:HaxeExpr, eelse:HaxeExpr) {
-    final s = "if " + t.translateExpr(econd) + " " + t.translateExpr(eif);
+    final s = "if " + t.translateExpr(econd) + " " + t.translateExpr(translateToBlock(eif));
     if (eelse != null) {
-        return s + " else " + t.translateExpr(eelse);
+        return s + " else " + t.translateExpr(translateToBlock(eelse));
     }else{
         return s;
     }
+}
+
+private function translateToBlock(expr:HaxeExpr):HaxeExpr {
+    if (expr.def == null)
+        return expr;
+    switch expr.def {
+        case EIf(_, _, _):
+        case EBlock(_):
+        default:
+            return {
+                t: expr.t,
+                specialDef: null,
+                def: EBlock([expr]),
+                remapTo: expr.remapTo,
+            };
+    }
+    return expr;
 }

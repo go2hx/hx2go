94543fa86f8cdfaf1eec6af2994ca3621475e032
Joey <9813078+PXshadow@users.noreply.github.com>
2026-01-12 20:37:01 +0100

    translator/exprs/Function: handle logic to turn a def or expr function into Go code!

--- source/translator/exprs/Function.hx
+++ source/translator/exprs/Function.hx
@@ -1,8 +1,41 @@
 package translator.exprs;
 
+import HaxeExpr.HaxeFunction;
 import translator.Translator;
-import HaxeExpr;
+import translator.TranslatorTools;
 
-function translateFunction(t:Translator, kind:FunctionKind, f:HaxeFunction) {
-    
-} 
+function translateFunction(t:Translator, name:String, f:HaxeFunction) {
+    final exprString = f.expr == null ? "{}" : t.translateExpr(f.expr);
+    final paramString = if (f.params != null && f.params.length > 0) {
+        // change out generic types to match with function
+        normalizeGenericTypes(name, f);
+        // string
+        "[" +
+        f.params.map(p -> p.name + " any").join(",") +
+        "]";
+    }else{
+        "";
+    }
+    final args = f.args.map(arg -> arg.name + " " + t.translateComplexType(arg.type));
+    return 'func $name$paramString(${args.join(", ")}) $exprString\n';
+}
+// TODO
+// Move to transformer
+// Normalize all potential generic types inside of the function body as well
+function normalizeGenericTypes(name:String, f:HaxeFunction) {
+    for (i in 0...f.args.length) {
+        switch f.args[i].type {
+            case TPath(p):
+                var isTypeParam = false;
+                for (param in f.params) {
+                    if (param.name == p.name && p.pack.length == 1 && p.pack[0] == toCamelCase(name)) {
+                        isTypeParam = true;
+                        break;
+                    }
+                }
+                if (isTypeParam)
+                    p.pack = [];
+            default:
+        }
+    }
+}

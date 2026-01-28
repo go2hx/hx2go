d4883e4af6698e26a1aeb9595a0ecb10818c2788
Joey <9813078+PXshadow@users.noreply.github.com>
2026-01-28 03:27:08 +0100

    testbed: update with anon struct get and set using non pure funcs

--- testbed/Test.hx
+++ testbed/Test.hx
@@ -1,9 +1,18 @@
 class Test {
     public static function main() {
         var x = {
-            x: Std.random(10),
+            x: foo(),
             y: 0.2,
         };
+        change(x);
+
         Sys.println(x.x);
     }
+
+    public static function change(x:{x:Int, y:Float}) {
+        x.x = 10;
+    }
+
+    public static function foo():Int
+        return 10;
 }

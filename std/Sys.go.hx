6c5591e062fa68841fdf97abf6c4c579ede3a5fe
Joey <9813078+PXshadow@users.noreply.github.com>
2026-02-06 20:40:56 +0100

    std: reformat Sys and add exit func

--- std/Sys.go.hx
+++ std/Sys.go.hx
@@ -1,16 +1,19 @@
 import go.Fmt;
 import go.time.Time;
+import go.Os;
 import go.Syntax;
-
+@:inherientDoc
 class Sys {
+	public static function println<T>(v:T):Void
+		Fmt.println(v);
 
-    public static function println<T>(v:T): Void
-        Fmt.println(v);
+	public static function print<T>(v:T):Void
+		Fmt.print(v);
 
-    public static function print<T>(v:T): Void
-        Fmt.print(v);
+	public static function sleep(seconds:Float):Void
+		return Time.sleep(Time.second * seconds);
 
-    public static function sleep(seconds: Float): Void
-        return Time.sleep(Time.second * seconds);
-
+	public static function exit(code:Int):Void {
+		Os.exit(code);
+	}
 }

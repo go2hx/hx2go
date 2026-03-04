d0c2121fb22a618e640a7abf3faa17272fd88889
mikaib <mikaib@duck.com>
2026-03-04 11:09:56 +0100

    fix 'x[y] = y++' and patch array access types

--- testbed/Test.hx
+++ testbed/Test.hx
@@ -16,10 +16,8 @@ class Test {
 
     static function main() {
         var arr: Dynamic = [1, 2, 3];
-        var x: Dynamic = 5;
-
         Sys.println(arr);
-        Sys.println(arr[0] * x);
+        Sys.println(arr[0] * arr[1]);
     }
 
 }

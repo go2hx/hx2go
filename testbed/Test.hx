5708188ef3f6ca8b764e83719952b9288e2b62c2
Joey <9813078+PXshadow@users.noreply.github.com>
2026-03-22 19:58:01 +0100

    testbed: new enum tests

--- testbed/Test.hx
+++ testbed/Test.hx
@@ -1,9 +1,22 @@
 import go.Fmt;
 
 function main() {
-    Fmt.println(new Foo());
+    Fmt.println(get().getName());
+    Fmt.println(get().getIndex());
+    Fmt.println(get2().getIndex());
 }
 
-class Foo {
-    public function new() {}
+class Foo2 {}
+
+function get()
+    return Foo.A;
+
+function get2()
+    return Foo.B;
+
+
+enum Foo {
+    A;
+    B;
+    C(f:Foo);
 }

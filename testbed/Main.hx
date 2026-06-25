ac6f9052df75f8313a031b379d9770f5b18f3558
mikaib <mikaib@duck.com>
2026-06-25 13:40:19 +0200

    test case unop

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,15 +1,20 @@
 class Main {
 
-    public static function main() {
-        var x: Int = 3;
-        var y: Int = 5;
-        var x_null: Null<Int> = x;
-        var y_null: Null<Int> = y;
+//    public static function max<T: Float>(a: T, b: T): T {
+//        return if (a > b) a else b;
+//    }
 
-        var test_a = x + y_null;
-        var test_b = x_null + y;
-        var test_c = x_null + y_null;
-        var test_d = x + y;
+    public static function main() {
+//        Sys.println(max(1, 3));
+//        Sys.println(max(10.1, 19.5));
+
+        var x: Int = 5;
+        var x_null: Null<Int> = x;
+        var x_dyn: Dynamic = x;
+
+        var x_neg: Int = -x;
+        var x_null_neg: Null<Int> = -x_null;
+        var x_dyn_neg: Dynamic = -x_dyn;
     }
 
 }

0ec701911e0179cd20407455add381a7e3296ff7
mikaib <mikaib@duck.com>
2026-06-26 00:07:34 +0200

    testcase for optional args

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,18 +1,12 @@
-import go.fmt.Fmt;
-
 class Main {
 
-    public static function max<T : Float>(a: T, b: T): T {
-        var foo: T = a > b ? a : b;
-        var bar: Null<T> = foo;
-
-        return bar;
+    public static function doStuff(a, b, ?c): Float {
+        return c == null ? a + b : (a + b) * c;
     }
 
     public static function main() {
-        trace(max(1, 3));
-        trace(max(10.1, 19.5));
-        trace(max(109.4, 13));
+        trace(doStuff(10, 40));
+        trace(doStuff(10, 40, 10));
     }
 
 }

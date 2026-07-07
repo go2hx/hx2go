0cd7d9e0de7549e7fc1b9154793e64318c15bea4
mikaib <mikaib@duck.com>
2026-07-07 22:48:10 +0200

    cpp compiler

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,34 +1,5 @@
-abstract Foo(Int) from Int {
+import go.fmt.Fmt;
 
-    public function log(): Void {
-        trace('foo logged', this);
-    }
-
-}
-
-abstract Bar<T>(Array<T>) from Array<T> to Array<T> {
-
-    public function logAll(): Void {
-        for (item in this) {
-            trace(item);
-        }
-    }
-
-    public function toArray(): Array<T> {
-        return this;
-    }
-
-}
-
-class Main {
-
-   public static function main(): Void {
-       var arr: Array<Int> = [1, 2, 3];
-       var abs1: Bar<Int> = arr;
-       var abs2: Bar<Foo> = abs1;
-       abs2.logAll();
-       var arr = abs2.toArray();
-       arr[0].log();
-   }
-
+function main() {
+    trace("Hello, World!");
 }

698ee53fd1704188ae4fc6fff24aa143d9d3c387
mikaib <mikaib@duck.com>
2026-07-11 00:58:53 +0200

    bring back enum testbed

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,7 +1,18 @@
-function main() {
-    var x: Int = 5;
-    var x_null: Null<Int> = x;
+private enum Colour {
+    Red;
+    Green;
+    Blue;
+    RGBA(r: Float, g: Float, b: Float, a: Float);
+}
 
-    trace(x_null == null);
-    trace(x_null == 3);
+class Foo {
+    public function new() {}
+}
+
+function main() {
+    var c1: Colour = Red;
+    var c2: Colour = RGBA(1, 0, 0, 1);
+    var c3: EnumValue = c2;
+
+    trace(c1, c2, c3, new Foo());
 }

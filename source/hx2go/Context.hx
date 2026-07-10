9a1210075d5db816765704559031fe5f5a72d3d0
mikaib <mikaib@duck.com>
2026-07-10 23:10:34 +0200

    keyword reservation

--- source/hx2go/Context.hx
+++ source/hx2go/Context.hx
@@ -26,7 +26,20 @@ import hx2go.util.TypeHelper;
 class Context {
 
     private static final _reserved: Map<String, Bool> = [
-        "func" => true
+        "break" => true, "default" => true, "func" => true, "interface" => true, "select" => true,
+        "case" => true, "defer" => true, "go" => true, "map" => true, "struct" => true,
+        "chan" => true, "else" => true, "goto" => true, "package" => true, "switch" => true,
+        "const" => true, "fallthrough" => true, "if" => true, "range" => true, "type" => true,
+        "continue" => true, "for" => true, "import" => true, "return" => true, "var" => true,
+        "any" => true, "bool" => true, "byte" => true, "comparable" => true,
+        "complex64" => true, "complex128" => true, "error" => true, "float32" => true, "float64" => true,
+        "int" => true, "int8" => true, "int16" => true, "int32" => true, "int64" => true,
+        "rune" => true, "string" => true,
+        "uint" => true, "uint8" => true, "uint16" => true, "uint32" => true, "uint64" => true, "uintptr" => true,
+        "true" => true, "false" => true, "iota" => true, "nil" => true,
+        "append" => true, "cap" => true, "close" => true, "complex" => true, "copy" => true,
+        "delete" => true, "imag" => true, "len" => true, "make" => true, "new" => true,
+        "panic" => true, "print" => true, "println" => true, "real" => true, "recover" => true,
     ];
 
     private var types: Map<String, HxbModuleType>;
@@ -109,7 +122,7 @@ class Context {
     }
 
     public function sanitiseString(v: String): String {
-        return v; // _reserved.exists(v) ? '_hx_reserved_$v' : v;
+        return _reserved.exists(v) ? '_hx_reserved_$v' : v;
     }
 
     public function buildType(t: HxbModuleType, ref: ModuleRef): Void {
--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,9 +1,5 @@
 import go.Syntax;
 
 function main() {
-    var x: Array<Dynamic> = [true, "Test"];
-    var y: Array<Int> = [1, 2];
-
-    var x2 = x.copy();
-    var y2 = y.copy();
+    var m: Map<String, Int> = [];
 }

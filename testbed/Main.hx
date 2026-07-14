4dc3a579c138a450619614cf71f243f5e3fce245
Joey <9813078+PXshadow@users.noreply.github.com>
2026-07-14 17:42:47 +0200

    testbed: add Chan select statement test with variable

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,58 +1,20 @@
+import go.Chan;
+import go.Syntax;
+
 function main() {
-        // Local-constructed date: 2021-06-15 10:30:45 local time
-        var d = new Date(2021, 5, 15, 10, 30, 45);
-        trace("=== new Date(2021,5,15,10,30,45) local ===");
-        trace("getFullYear=" + d.getFullYear());
-        trace("getMonth=" + d.getMonth());
-        trace("getDate=" + d.getDate());
-        trace("getHours=" + d.getHours());
-        trace("getMinutes=" + d.getMinutes());
-        trace("getSeconds=" + d.getSeconds());
-        trace("getDay=" + d.getDay());
-        trace("getTime=" + d.getTime());
-        trace("getTimezoneOffset=" + d.getTimezoneOffset());
-        trace("toString=" + d.toString());
-
-        trace("=== UTC getters ===");
-        trace("getUTCFullYear=" + d.getUTCFullYear());
-        trace("getUTCMonth=" + d.getUTCMonth());
-        trace("getUTCDate=" + d.getUTCDate());
-        trace("getUTCHours=" + d.getUTCHours());
-        trace("getUTCMinutes=" + d.getUTCMinutes());
-        trace("getUTCSeconds=" + d.getUTCSeconds());
-        trace("getUTCDay=" + d.getUTCDay());
-
-        // fromTime: 2021-06-15 12:00:00 UTC = 1623758400 s = 1623758400000 ms
-        var ft = Date.fromTime(1623758400000.0);
-        trace("=== fromTime(1623758400000) ===");
-        trace("getTime=" + ft.getTime());
-        trace("getUTCFullYear=" + ft.getUTCFullYear());
-        trace("getUTCMonth=" + ft.getUTCMonth());
-        trace("getUTCHours=" + ft.getUTCHours());
-        trace("getUTCMinutes=" + ft.getUTCMinutes());
-        trace("getUTCSeconds=" + ft.getUTCSeconds());
-        trace("getUTCDay=" + ft.getUTCDay());
-        trace("toString=" + ft.toString());
-
-        // fromString
-        var fs = Date.fromString("2021-06-15 10:30:45");
-        trace("=== fromString('2021-06-15 10:30:45') ===");
-        trace("getFullYear=" + fs.getFullYear());
-        trace("getMonth=" + fs.getMonth());
-        trace("getHours=" + fs.getHours());
-        trace("getMinutes=" + fs.getMinutes());
-        trace("getSeconds=" + fs.getSeconds());
-        trace("toString=" + fs.toString());
-
-        var fs2 = Date.fromString("2021-06-15");
-        trace("=== fromString('2021-06-15') ===");
-        trace("getFullYear=" + fs2.getFullYear());
-        trace("getMonth=" + fs2.getMonth());
-        trace("toString=" + fs2.toString());
-
-        // now
-        var n = Date.now();
-        trace("=== now ===");
-        trace("getFullYear=" + n.getFullYear());
-        trace("getTime>0=" + (n.getTime() > 0));
+    var c = new Chan<Int>(1);
+    c.send(1);
+    Syntax.select(Select.receive(c, (value) -> {
+        trace(value);
+    }));
+    var l = new sys.thread.Lock();
+    trace(l.wait());
+    Syntax.go(() -> {
+        l.wait(2);
+        trace("finish");
+        l.release();
+    });
+    Sys.sleep(1);
+    l.release();
+    Sys.sleep(0.1);
 }

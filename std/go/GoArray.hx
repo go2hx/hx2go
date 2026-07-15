641bab3898ae74cebc47ebab40112773814bb821
mikaib <mikaib@duck.com>
2026-07-15 22:57:29 +0200

    more methods for go.GoArray

--- std/go/GoArray.hx
+++ std/go/GoArray.hx
@@ -4,6 +4,11 @@ package go;
 @:runtimeValue
 extern abstract GoArray<T, @:const Size: Int> {
 
+    public var length(get, never): GoInt;
+    private inline function get_length(): GoInt {
+        return Go.len(this);
+    }
+
     // note: marked as extern to force inline
     @:arrayAccess @:pure private extern inline function get(index: Int): T {
         return Syntax.code("{0}[{1}]", this, (index : Int));
@@ -17,6 +22,22 @@ extern abstract GoArray<T, @:const Size: Int> {
 
     @:to public inline function toSlice(): Slice<T> {
         return Syntax.code("{0}[:]", this);
+    }
+
+    @:pure public inline extern overload function slice(low: GoInt, high: GoInt, max: GoInt): Slice<T> {
+        return Syntax.code("{0}[{1}:{2}:{3}]", this, (low : GoInt), (high : GoInt), (max : GoInt));
+    }
+
+    @:pure public inline extern overload function slice(low: GoInt, high: GoInt): Slice<T> {
+        return Syntax.code("{0}[{1}:{2}]", this, (low : GoInt), (high : GoInt));
+    }
+
+    @:pure public inline extern overload function slice(low: GoInt): Slice<T> {
+        return Syntax.code("{0}[{1}:]", this, (low : GoInt));
+    }
+
+    @:pure public inline extern overload function sliceEnd(high: GoInt): Slice<T> {
+        return Syntax.code("{0}[:{1}]", (this : Slice<T>), (high : GoInt));
     }
 
 }

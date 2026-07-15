563434ef28d648c2812a5ba10247b17dc3923edb
mikaib <mikaib@duck.com>
2026-07-15 12:20:07 +0200

    bring back old tests

--- tests/unit/ArrayCastVariants.hx
+++ tests/unit/ArrayCastVariants.hx
@@ -2,14 +2,14 @@ package unit;
 
 
 function main() {
-    // var test: Array<Int> = [1, 2, 3];
-    // var test_dyn_arr: Array<Dynamic> = test;
-    // var test_dyn_obj: Dynamic = test;
+     var test: Array<Int> = [1, 2, 3];
+     var test_dyn_arr: Array<Dynamic> = test;
+     var test_dyn_obj: Dynamic = test;
 
-    // assert(test[0] == 1);
-    // assert(test.length == 3);
-    // assert(test_dyn_arr[0] == 1);
-    // assert(test_dyn_arr.length == 3);
-    // assert(test_dyn_obj[0] == 1);
-    // assert(test_dyn_obj.length == 3);
+     assert(test[0] == 1);
+     assert(test.length == 3);
+     assert(test_dyn_arr[0] == 1);
+     assert(test_dyn_arr.length == 3);
+     assert(test_dyn_obj[0] == 1);
+     assert(test_dyn_obj.length == 3);
 }

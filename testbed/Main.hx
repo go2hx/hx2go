3ecaca8b64197954ee3cdb2283aa5359032303a7
Joey <9813078+PXshadow@users.noreply.github.com>
2026-07-16 19:37:38 +0200

    revert testbed to use map test

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,19 +1,4 @@
-import haxe.crypto.Sha512;
-import haxe.io.Bytes;
-import haxe.ds.Vector;
-
 function main() {
-    var garbage = "";
-    for (i in 0...2048) {
-        garbage += String.fromCharCode(41 + Std.int(Math.random() * 26));
-    }
-
-    var start = Sys.time();
-    for (i in 0...10000) {
-        Sha512.encode(garbage);
-    }
-    var end = Sys.time();
-
-    trace('took ${end - start}s');
-
+    var m: Map<Int, String> = [10 => "ten"];
+    trace(m[10]);
 }

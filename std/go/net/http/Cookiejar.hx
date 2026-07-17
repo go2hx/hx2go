abc27e230d00659bc46d1a017f29c3852b105ee5
Joey <9813078+PXshadow@users.noreply.github.com>
2026-07-17 13:53:59 +0200

    Remove case sensetive variant of CookieJar

--- std/go/net/http/Cookiejar.hx
+++
@@ -1,8 +0,0 @@
-package go.net.http;
-
-@:go.Type({ name: "cookiejar", instanceName: "cookiejar.cookiejar", imports: ["net/http/cookiejar"] })
-extern class Cookiejar {
-
-    @:native("New") static function _new(o: go.Pointer<go.net.http.cookiejar.Options>): go.Result<go.Pointer<go.net.http.cookiejar.Jar>>;
-
-}

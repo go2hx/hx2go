4f80d1bdd3fd1038e70cd2a65acb996e6d7e07c1
Joey <9813078+PXshadow@users.noreply.github.com>
2026-07-17 20:28:24 +0200

    add unit test by setting define -D test=Example

---
+++ testbed/AddTest.macro.hx
@@ -0,0 +1,16 @@
+import haxe.macro.Context;
+import sys.io.File;
+import sys.FileSystem;
+import haxe.macro.Compiler;
+
+function init() {
+    var testName = Context.getDefines()["test"];
+    if (testName == null || testName == "")
+        return;
+    var path = "tests/unit/" + testName + ".hx";
+    if (FileSystem.exists(path)) {
+        return;
+    }
+
+    File.saveContent(path, File.getContent("testbed/Main.hx"));
+}

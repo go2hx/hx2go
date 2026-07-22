967c5a7e4973adcee8f9d8eecfe33fc699b0a8b7
Joey <9813078+PXshadow@users.noreply.github.com>
2026-07-22 17:01:43 +0200

    scripts: NeededImports use new output directories to get imports for bootstrap and tests, pass -stdgen flag which prevents golang.org libs being brought in except term and tools

--- scripts/NeededImports.hx
+++ scripts/NeededImports.hx
@@ -1,3 +1,4 @@
+import sys.io.File;
 import haxe.DynamicAccess;
 import haxe.Json;
 import sys.io.Process;
@@ -9,11 +10,12 @@ var root = "";
 function main() {
     root = Sys.getCwd();
     var imps:Array<String> = [];
-    getImports(imps, "./output/main");
+    getImports(imps, "./output/bootstrap/main");
+    getImports(imps, "./output/tests/main");
 
     var dir = runCommandReturnLine("haxelib", ["path", "go2hx"]);
     getImports(imps, Path.join([dir, "..", "/output/main"]));
-    Sys.command('haxelib run go2hx ${imps.join(" ")} ./std');
+    Sys.command('haxelib run go2hx -stdgen ${imps.join(" ")} ./std');
 }
 
 function runCommand(cmd:String, args:Array<String>):String {

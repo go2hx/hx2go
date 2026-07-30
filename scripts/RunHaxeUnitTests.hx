d0fa25e3d41912f00fb1d6c4a5c971f6560e524e
Joey <9813078+PXshadow@users.noreply.github.com>
2026-07-30 17:09:23 +0200

    haxe scripts/haxeunit runs the Haxe compiler test suite for the Go target

---
+++ scripts/RunHaxeUnitTests.hx
@@ -0,0 +1,41 @@
+import sys.FileSystem;
+
+function main() {
+    if (!FileSystem.exists("haxe")) {
+        Sys.command("git clone -b go-target https://github.com/pxshadow/haxe");
+    }
+    Sys.setCwd("haxe");
+    Sys.command("git pull");
+    Sys.setCwd("tests/unit");
+    // runTestSuite("macro");
+    runTestSuite("go");
+}
+
+function runTestSuite(target:String) {
+    var args = ['compile-$target.hxml'];
+    var defines:Map<String,String> = getDefines();
+    for (key => value in defines) {
+        if (key.indexOf("target") != -1)
+            continue;
+        switch key {
+            case "eval", "true", "sys", "haxe-ver", "haxe_ver", "haxe":
+                continue;
+        }
+        args.push("-D");
+        if (value == "1") {
+            args.push(key);
+        }else{
+            args.push(key + "=" + value);
+        }
+    }
+    Sys.println("haxe " + args.join(" "));
+    Sys.command('haxe', args);
+}
+
+#if macro
+macro function getDefines():haxe.macro.Expr.ExprOf<Map<String,String>> {
+    return macro $v{haxe.macro.Context.getDefines()};
+}
+#else
+macro function getDefines():haxe.macro.Expr.ExprOf<Map<String,String>>;
+#end

82a433564cd059adeed3b17177a4a3654a75d186
Joey <9813078+PXshadow@users.noreply.github.com>
2026-08-04 16:09:14 +0200

    passes: FloatMod support OpAssignOp(OpMod)

--- source/hx2go/passes/FloatMod.hx
+++ source/hx2go/passes/FloatMod.hx
@@ -13,20 +13,31 @@ class FloatMod extends CompilerPass {
 
     public function match(expr: HxbTypedExpr): Bool {
         return switch expr.expr {
-            case TBinop(OpMod, _, _) if (Semantics.isFloatType(context, expr.t)): true;
+            case TBinop(OpAssignOp(OpMod) | OpMod, _, _): Semantics.isFloatType(context, expr.t);
             case _: false;
         }
     }
 
     public function execute(expr: HxbTypedExpr, frame: ContextFrame): Void {
+        var op = null;
+        var right = null;
+        var left = null;
         var o = switch expr.expr {
-            case TBinop(_, b_left, b_right):
+            case TBinop(b_op, b_left, b_right):
+                op = b_op;
+                right = b_right;
+                left = b_left;
                 context.defineImport(frame, "math");
-                ExprHelper.createUntyped('math.Mod({0}, {1})', [b_left, b_right]);
+                ExprHelper.createUntyped('math.Mod({0}, {1})', [b_left, Copy.copy(b_right)]);
             case _: expr;
         }
-        expr.expr = o.expr;
-        expr.t = o.t;
+        if (op.match(OpAssignOp(OpMod))) {
+            expr.expr = TBinop(OpAssign, Copy.copy(left), o);
+            context.submitNode(expr, true);
+        }else{
+            expr.expr = o.expr;
+            expr.t = o.t;
+        }
     }
 
 }

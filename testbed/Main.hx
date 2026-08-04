6e309eda43b5b6d36d1648170ca36e452668ddf6
mikaib <mikaib@duck.com>
2026-08-04 15:42:19 +0200

    HTTP compiles milestone

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,29 +1,7 @@
-//import haxe.Http;
-//
-//function main() {
-//    trace(
-//        Http.requestUrl("http://raw.githubusercontent.com/go2hx/hx2go/refs/heads/main/README.md")
-//    );
-//}
+import haxe.Http;
 
-@:structInit
-class Foo {
-    public var v:Int = 0;
-
-    public dynamic function log(mul: Float) {
-        trace("Value is", this.v * mul);
-    }
-}
-
-class Main {
-    static function main() {
-        var a: Foo = { v : 1 };
-        var b: Foo = { v : 2 };
-
-        a.log(2);
-        b.log(2);
-        a.log = b.log;
-        a.log(2);
-        b.log(2);
-    }
+function main() {
+    trace(
+        Http.requestUrl("http://raw.githubusercontent.com/go2hx/hx2go/refs/heads/main/README.md")
+    );
 }

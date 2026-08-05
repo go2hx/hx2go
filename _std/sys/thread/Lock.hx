e98f8e7ccd9a030ab0ce6ba34573d353af510d71
Joey <9813078+PXshadow@users.noreply.github.com>
2026-08-05 20:37:41 +0200

    Lock: that keeps track if it's been acquired

--- _std/sys/thread/Lock.hx
+++ _std/sys/thread/Lock.hx
@@ -26,77 +26,35 @@ import go.Time_ as Time;
 import go.Go;
 import go.Syntax;
 
-/**
-	A Lock allows blocking execution until it has been unlocked. It keeps track
-	of how often `release` has been called, and blocks exactly as many `wait`
-	calls.
-
-	The order of the `release` and `wait` calls is irrelevant. That is, a Lock
-	can be released before anyone waits for it. In that case, the `wait` call
-	will execute immediately.
-
-	Usage example:
-
-	```haxe
-	var lock = new Lock();
-	var elements = [1, 2, 3];
-	for (element in elements) {
-		// Create one thread per element
-		new Thread(function() {
-			trace(element);
-			Sys.sleep(1);
-			// Release once per thread = 3 times
-			lock.release();
-		});
-	}
-	for (_ in elements) {
-		// Wait 3 times
-		lock.wait();
-	}
-	trace("All threads finished");
-	```
-**/
 @:coreApi
 class Lock {
-    var c: go.Chan<Bool>;
-	/**
-		Creates a new Lock which is initially locked.
-	**/
+	var c:go.Chan<Bool>;
 	public function new():Void {
 		c = new go.Chan(1);
-		c.send(true);
 	}
 
-	/**
-		Waits for the lock to be released, or `timeout` (in seconds)
-		to expire. Returns `true` if the lock is released and `false`
-		if a time-out occurs.
-	**/
 	public function wait(?timeout:Float):Bool {
-        if (timeout == null) {
-            return c.tryReceive().ok;
+		if (timeout == null) {
+			c.receive();
+			return true;
 		}
-		
+
 		var acquired = false;
-		Syntax.select(
-			Select.receive(c, (_) -> {
-				acquired = true;
-			}),
-			Select.receive(Time.after(Go.int64(timeout * 1e9)), (_) -> {
-				acquired = false;
-			})
-		);
+		Syntax.select(Select.receive(c, (_) -> {
+			acquired = true;
+		}), Select.receive(Time.after(Go.int64(timeout * 1e9)), (_) -> {
+			acquired = false;
+		}));
 		return acquired;
-    }
+	}
 
-	/**
-		Releases the lock once.
-
-		The thread does not need to own the lock in order to release
-		it. Each call to `release` allows exactly one call to `wait`
-		to execute.
-	**/
 	public function release():Void {
-        c.send(true);
-    }
+		var sent = false;
+		Syntax.select(Select.send(c, true, () -> {
+			sent = true;
+		}), Select.defaultCase(() -> {}));
+		if (sent)
+			return;
+		Syntax.go(() -> c.send(true));
+	}
 }

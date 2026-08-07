e90d65590ebba1567b95271844fd3ef49e0cbcd8
Joey <9813078+PXshadow@users.noreply.github.com>
2026-08-07 20:24:56 +0200

    fix case insensivity drift for Go's stdlib regen

--- std/go/crypto/hpke/Aead_.hx
+++ std/go/crypto/hpke/Aead_.hx
@@ -2,7 +2,7 @@ package go.crypto.hpke;
 
 @:structInit
 @:go.Type({ name: "aead", instanceName: "hpke.aead", imports: ["crypto/hpke"] })
-extern class Aead {
+extern class Aead_ {
 
     @:native("ID") function ID(): (go.UInt16);
 
--- std/go/net/http/Http2clientConnPool_.hx
+++ std/go/net/http/Http2clientConnPool_.hx
@@ -2,7 +2,7 @@ package go.net.http;
 
 @:structInit
 @:go.Type({ name: "http2clientConnPool", instanceName: "http.http2clientConnPool", imports: ["net/http"] })
-extern class Http2clientConnPool {
+extern class Http2clientConnPool_ {
 
     @:native("GetClientConn") function getClientConn(req: go.Pointer<go.net.http.Request>, addr: String): (go.Result<go.Pointer<go.net.http.Http2ClientConn>>);
     @:native("MarkDead") function markDead(cc: go.Pointer<go.net.http.Http2ClientConn>): Void;

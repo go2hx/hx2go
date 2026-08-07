159002b1033989b36c74a98bdac7440c8c28fa2c
Joey <9813078+PXshadow@users.noreply.github.com>
2026-08-08 01:44:51 +0200

    Main: support for haxe.Resource

--- source/hx2go/Main.hx
+++ source/hx2go/Main.hx
@@ -1,5 +1,7 @@
 package hx2go;
 
+import haxe.ds.Map;
+import haxe.io.Bytes;
 import haxe.io.Path;
 import sys.FileSystem;
 import hxb.Hxb;
@@ -17,38 +19,40 @@ class Main {
         var mainClass = args[2] ?? "Main";
         var singleFile = (args[3] == "1") ?? false;
         var sourcelineComments = (args[4] == "1") ?? false;
+        var resSerialized = args[5];
+        var res:Map<String, Bytes> = haxe.Unserializer.run(resSerialized);
 
         // accept both absolute paths (-D go-bootstrap) and relative paths
         var absoluteOutput = Path.isAbsolute(relativeOutput) ? relativeOutput : Path.join([ root, relativeOutput ]);
         var absoluteInput = Path.isAbsolute(relativeInput) ? relativeInput : Path.join([ root, relativeInput ]);
 
-        exec(absoluteInput, absoluteOutput, mainClass, singleFile, sourcelineComments);
+        exec(absoluteInput, absoluteOutput, mainClass, singleFile, sourcelineComments, res);
     }
 
     public static function importToPath(imp: HxbImport): String {
         return imp.pack.length > 0 ? '${imp.pack.join(".")}.${imp.name}' : imp.name;
     }
 
-    public static function exec(input: String, output: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool): Void {
+    public static function exec(input: String, output: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool, res:Map<String, Bytes>): Void {
         final start = Sys.time();
         if (!FileSystem.exists(input)) {
             Sys.println("HXB not found: " + input);
             Sys.exit(1);
         }
         var arc = Hxb.loadArchive(input);
-        generate(arc, output, mainClass, singleFile, sourcelineComments);
+        generate(arc, output, mainClass, singleFile, sourcelineComments, res);
 
         final end = Sys.time();
         Sys.println('hx2go took ${Std.string(Math.round((end - start) * 100000) / 100)}ms');
     }
 
-    public static function generate(archive: HxbArchive, absoluteOutput: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool): Void {
+    public static function generate(archive: HxbArchive, absoluteOutput: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool, res:Map<String, Bytes>): Void {
         if (!FileSystem.exists(absoluteOutput)) {
             FileSystem.createDirectory(absoluteOutput);
         }
 
         var ctx = new hx2go.Context(archive, absoluteOutput, sourcelineComments);
-        ctx.build(mainClass, singleFile);
+        ctx.build(mainClass, singleFile, res);
     }
 
 }

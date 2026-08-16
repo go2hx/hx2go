e9703904e5b2af558e9b7be353a92be969a65411
Joey <9813078+PXshadow@users.noreply.github.com>
2026-08-16 15:32:03 +0200

    tests/unit: function compare

--- tests/unit/FunctionCompare.hx
+++ tests/unit/FunctionCompare.hx
@@ -8,9 +8,18 @@ function main() {
 	assert((foo : Func) != (foo2 : Func));
 	assert((foo : Null<Func>) != (foo2 : Func));
 	assert((foo : Null<Func>) == (foo : Func));
+	var cl1 = new Cl();
+	var cl2 = new Cl();
+	trace(cl1.method == cl1.method);
+	trace(cl1.method != cl2.method);
 }
 
 typedef Func = Void -> Void;
 
 function foo() {}
 function foo2() {}
+
+class Cl {
+	public function new() {}
+	public function method() {}
+}

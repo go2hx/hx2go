8ec29ad269d42b2daf4490abb9fbc63a0d3b742c
Joey <9813078+PXshadow@users.noreply.github.com>
2026-08-17 17:14:17 +0200

    Main/Context: add hxb times signal

--- source/hx2go/Main.hx
+++ source/hx2go/Main.hx
@@ -38,20 +38,26 @@ class Main {
         return imp.pack.length > 0 ? '${imp.pack.join(".")}.${imp.name}' : imp.name;
     }
 
-    public static function exec(input: String, output: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool, res:haxe.ds.Map<String, Bytes>, times:Bool = false): Void {
+    public static function exec(input: String, output: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool, res:haxe.ds.Map<String, Bytes>, timesBool:Bool = false): Void {
         final start = Sys.time();
         if (!FileSystem.exists(input)) {
             Sys.println("HXB not found: " + input);
             Sys.exit(1);
         }
+
+        var times = new hx2go.util.Times(timesBool);
+
+        var closeHxb = times.start("hxb");
         var arc = Hxb.loadArchive(input);
+        closeHxb();
+
         generate(arc, output, mainClass, singleFile, sourcelineComments, res, times);
 
         final end = Sys.time();
         Sys.println('hx2go took ${Std.string(Math.round((end - start) * 100000) / 100)}ms');
     }
 
-    public static function generate(archive: HxbArchive, absoluteOutput: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool, res:haxe.ds.Map<String, Bytes>, times:Bool = false): Void {
+    public static function generate(archive: HxbArchive, absoluteOutput: String, mainClass: String, singleFile:Bool, sourcelineComments:Bool, res:haxe.ds.Map<String, Bytes>, times:hx2go.util.Times): Void {
         if (!FileSystem.exists(absoluteOutput)) {
             FileSystem.createDirectory(absoluteOutput);
         }

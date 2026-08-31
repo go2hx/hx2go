1215ae3cade0afec4fba8ed07d7d851ea1a024ac
Joey <joeypxshadow@duck.com>
2026-08-31 09:52:05 -0700

    ExprWriter: change from map[string]any to any(struct{ with Hx_Field prefixes for the fields

--- source/hx2go/writers/ExprWriter.hx
+++ source/hx2go/writers/ExprWriter.hx
@@ -371,20 +371,26 @@ class ExprWriter extends WriterImpl {
 
     public function writeObjectDecl(expr: HxbTypedExpr, fields: Array<HxbTObjectField>): OutputBuffer {
         var buf = new OutputBuffer();
-        buf.addInline('any(map[string]any{ ');
+        buf.addInline('any(struct {');
 
         for (i in 0...fields.length) {
             var field = fields[i];
+            buf.addInline('Hx_Field_${field.name} any');
+            buf.add('');
+        }
 
-            buf.addInline('"${field.name}": ');
+        buf.addInline('}{');
+
+        for (i in 0...fields.length) {
+            var field = fields[i];
+            buf.addInline('Hx_Field_${field.name}: ');
             buf.addBufferInline(writeExpr(field.expr));
-
             if (i < fields.length - 1) {
                 buf.addInline(', ');
             }
         }
 
-        buf.addInline(' })');
+        buf.addInline('})');
 
         return buf;
     }

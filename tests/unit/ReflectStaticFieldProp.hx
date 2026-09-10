5ae00b5f7f67cb02987c1e671c7ffc9377d6d7fa
Joey <9813078+PXshadow@users.noreply.github.com>
2026-09-10 21:49:02 +0200

    tests/unit: fix baseline

--- tests/unit/ReflectStaticFieldProp.hx
+++ tests/unit/ReflectStaticFieldProp.hx
@@ -5,24 +5,18 @@ function main() {
 	assert(Reflect.fields(c).length == 0);
 	// static var
 	// look up
-	var cl = Type.resolveClass("ClassWithProp");
-	assert(Reflect.field(cl, "STAT_X") == null);
-	var fail = false;
-	try {
-		Reflect.setField(cl, "STAT_X", 11);
-	}catch(_) {
-		fail = true;
-	}
-	assert(fail);
-	assert(Reflect.field(cl, "STAT_X") == null);
-	assert(Reflect.fields(cl).length == 0);
+	var cl = Type.resolveClass("unit.ClassWithProp");
+	assert(Reflect.field(cl, "STAT_X") == 6);
+	Reflect.setField(cl, "STAT_X", 11);
+	assert(Reflect.field(cl, "STAT_X") == 11);
+	assert(Reflect.fields(cl).length > 0);
 	// access directly
 	assert(Reflect.field(ClassWithProp, "STAT_Y") == 10);
 	assert(Reflect.hasField(ClassWithProp, "STAT_Y"));
 	Reflect.setField(ClassWithProp, "STAT_Y", 11);
 	assert(Reflect.field(ClassWithProp, "STAT_Y") == 11);
 	// static prop
-	assert(ClassWithProp.STAT_X == 6);
+	assert(ClassWithProp.STAT_X == 11);
 
 	Reflect.setProperty(ClassWithProp, "STAT_X", 8);
 

f6109564e44ce3ebbe08a70b8aa974513e1c25cd
Joey <9813078+PXshadow@users.noreply.github.com>
2026-09-11 18:21:25 +0200

    runtime/std: allow Array's to be coerced into the correct type when using HxDynamic.convertToType (by registering array constructors of the given type) tryHxEnum in the same way as tryHxClass for just the constructor field

--- runtime/Array.go
+++ runtime/Array.go
@@ -4,7 +4,36 @@ import (
 	"reflect"
 	"slices"
 	"strings"
+	"sync"
 )
+
+// used to build a dynamic array
+var hxArrayConstructors sync.Map
+
+func hxRegisterArrayConstructor[T any]() {
+	target := reflect.TypeOf((*HxArray[T])(nil)).Elem()
+	if _, ok := hxArrayConstructors.Load(target); !ok {
+		hxArrayConstructors.Store(target, func(src HxArrayDyn) any { return HxArrayView[T]{src} })
+	}
+}
+
+func HxCoerceArray(val any, target reflect.Type) any {
+	src, ok := val.(HxArrayDyn)
+	if !ok {
+		return nil
+	}
+
+	if reflect.TypeOf(val).Implements(target) {
+		return nil
+	}
+
+	ctor, ok := hxArrayConstructors.Load(target)
+	if !ok {
+		return nil
+	}
+
+	return ctor.(func(HxArrayDyn) any)(src)
+}
 
 type HxArrayDyn interface {
 	Set_Dyn(idx int32, val any)
@@ -49,6 +78,7 @@ type HxArrayImpl[T any] struct {
 func HxMakeArray[T any](items ...T) HxArray[T] {
 	local := make([]T, len(items))
 	copy(local, items)
+	hxRegisterArrayConstructor[T]()
 	return &HxArrayImpl[T]{local}
 }
 
@@ -242,6 +272,7 @@ type HxArrayView[T any] struct {
 }
 
 func HxMakeArrayView[T any](src HxArrayDyn) HxArray[T] {
+	hxRegisterArrayConstructor[T]()
 	return HxArrayView[T]{src}
 }
 

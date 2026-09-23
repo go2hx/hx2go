e52b1a95d2f8d7431f85aac6fafab9d2878226e5
Joey <9813078+PXshadow@users.noreply.github.com>
2026-09-23 13:15:32 +0200

    tests/unit: Rtti1 make main public func

--- tests/unit/Rtti1.hx
+++ tests/unit/Rtti1.hx
@@ -2,7 +2,7 @@ package unit;
 @:rtti
 class Rtti1 {
   var x:String;
-  static function main() {
+  public static function main() {
     var rtti = haxe.rtti.Rtti.getRtti(Rtti1);
     assert(rtti.path == "Rtti1");
     assert(rtti.fields.length == 1);

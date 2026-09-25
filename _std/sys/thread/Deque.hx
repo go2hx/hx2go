7b7957440eafbac4698d48ddf0cfafd2a9e98828
mikaib <mikaib@duck.com>
2026-09-25 19:27:36 +0200

    deque

--- _std/sys/thread/Deque.hx
+++ _std/sys/thread/Deque.hx
@@ -1,14 +1,64 @@
 package sys.thread;
-// TODO implement
-@:coreApi class Deque<T> {
-	public function new():Void {}
-	public function add(i:T):Void {
+import go.Chan;
+import go.Syntax;
 
-    }
-	public function push(i:T):Void {
+@:coreApi
+class Deque<T> {
 
+    private var mutex: go.sync.Mutex;
+    private var items: Array<T>;
+    private var chan: Chan<Bool>;
+
+	public function new(): Void {
+        items = [];
+        chan = new Chan(1);
     }
-	public function pop(block:Bool):Null<T> {
-        return null;
+
+	public function add(i: T): Void {
+        mutex.lock();
+        items.push(i);
+        mutex.unlock();
+
+        notify();
     }
+
+	public function push(i: T): Void {
+        mutex.lock();
+        items.unshift(i);
+        mutex.unlock();
+
+        notify();
+    }
+
+	public function pop(block: Bool): Null<T> {
+        while (true) {
+            mutex.lock();
+            if (items.length > 0) {
+                var v = items.shift();
+                var hasItems = items.length > 0;
+                mutex.unlock();
+
+                if (hasItems) {
+                    notify();
+                }
+
+                return v;
+            }
+
+            mutex.unlock();
+
+            if (!block) return null;
+            chan.receive();
+        }
+
+        return null; // never reached, but needed for IntelliJ
+    }
+
+    private function notify(): Void {
+        Syntax.select(
+            Select.send(chan, true, () -> {}),
+            Select.defaultCase(() -> {})
+        );
+    }
+
 }
